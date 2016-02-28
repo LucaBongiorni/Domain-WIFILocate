56a494f4173a60c1789613157242b51a807c8eb6
Alex Rymdeko-Harvey <iamfree2009@gmail.com>
2016-02-28 17:17:35 -0500

    Intial Commit

---
+++ Common/kmlBuilder.py
@@ -0,0 +1,24 @@
+import simplekml 
+from Modules.Common import *
+
+
+class kml():
+
+  def __init__(self):
+    print helpers.color("[*] Started KML", bold=False)
+
+  def build(self, geo_lat, geo_long, ssid, desc_data=""): #Pass SSID name
+    #Build the instance
+    kml = simplekml.Kml()
+    #start the Point data.
+    pnt = kml.newpoint(name=ssid, coords=[(geo_lat, geo_long)])  # lon, lat, optional height
+    #If you pass description data, place it into the point.
+    if desc_data != "":
+        pnt.description = desc_data
+    kml.save("output2.kml")
+
+
+
+
+    
+
